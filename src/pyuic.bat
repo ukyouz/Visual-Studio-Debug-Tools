2850a828bc7feecec41cc9afc533d2902770958b
Chihhung Cheng <chihhung_cheng@tel.com>
2024-01-26 18:53:43 +0900

    fix venv path

--- src/pyuic.bat
+++ src/pyuic.bat
@@ -1,4 +1,4 @@
-@REM .\.venv\Lib\site-packages\qt6_applications\Qt\bin\rcc.exe -g python .\view\resource.qrc | sed '0,/PySide6/s//PyQt6/' > .\view\resource.py
+@REM ..\venv\Lib\site-packages\qt6_applications\Qt\bin\rcc.exe -g python .\view\resource.qrc | sed '0,/PySide6/s//PyQt6/' > .\view\resource.py
 pyuic6.exe .\view\BinView.ui -o .\view\BinView.py
 pyuic6.exe .\view\WidgetBinParser.ui -o .\view\WidgetBinParser.py
 pyuic6.exe .\view\VSdebugger.ui -o .\view\VSdebugger.py
