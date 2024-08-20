74f18246c5bfba41630f0c2494f98a01aa0f59bc
231814 <chihhung.cheng@tel.com>
2024-08-20 10:16:26 +0900

    improve pyinstaller.bat

--- src/pyinstaller.bat
+++ src/pyinstaller.bat
@@ -1,5 +1,6 @@
 @REM @echo off
 
+SET VER=%1
 
 py -m pytest tests --cov || goto :error
 coverage html
@@ -18,3 +19,19 @@ COPY dist\pyqode_backend\pyqode_backend.exe dist\VSdebugger\_internal
 
 pyinstaller.exe --noconfirm .\guiBinView.spec
 COPY dist\BinViewer\BinViewer.exe dist\VSdebugger
+
+if [%VER%] == [] SET VER=test
+
+echo %VER%
+
+py add_version_txt.py %VER%
+COPY about_me.txt dist\VSdebugger
+COPY ..\CHANGELOG.md dist\VSdebugger
+
+"C:\Program Files\7-Zip\7z.exe" a -t7z .\dist\VSdebugger_%VER%.7z .\dist\VSdebugger\
+
+@REM Restore My App Settings
+COPY app.ini dist\VSdebugger
+
+:error
+exit /b %ERRORLEVEL%add log view in VSDebugger
